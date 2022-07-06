55555fd6a2677e9fdc98ffd7b1f558b7d22286c4
vitor <vitory.rsilva@hotmail.com>
2022-07-06 20:26:38 -0300

    implementing eureka client

--- src/main/kotlin/com/ead/course/configs/RestTemplateConfig.kt
+++ src/main/kotlin/com/ead/course/configs/RestTemplateConfig.kt
@@ -1,12 +1,15 @@
 package com.ead.course.configs
 
 import org.springframework.boot.web.client.RestTemplateBuilder
+import org.springframework.cloud.client.loadbalancer.LoadBalanced
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.client.RestTemplate
 
 @Configuration
 class RestTemplateConfig {
+
+    @LoadBalanced
     @Bean
     fun restTemplate(builder: RestTemplateBuilder): RestTemplate {
         // Do any additional configuration here
